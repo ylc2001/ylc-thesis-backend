6c574aa8c6fc78808d48f4ebfefc9c2aedd607fb
ylc2001 <ylc20@mails.tsinghua.edu.cn>
2024-04-26 20:38:43 +0800

    ..

--- visualize_computation_graph.py
+++ visualize_computation_graph.py
@@ -4,14 +4,14 @@ from pyvis.network import Network
 
 # 说明：设置好输入 json 文件和输出的 html 文件名称
 
-file_name = "0424_cwh_correct.yaml_graph.json"
+file_name = "standard_graph.json"
 file_path = os.path.join(os.getcwd(), "results", file_name)
 output_name = "计算图.html"
 
 # 读取并解析json文件
 with open(file_path, 'r') as file:
     data = json.load(file)
-print(json.dumps(data, indent=4, ensure_ascii=False))
+# print(json.dumps(data, indent=4, ensure_ascii=False))
 
 net = Network(height='1000px', width='95%', directed=True)
 for item in data['nodes']:
@@ -51,6 +51,9 @@ for item in data['nodes']:
 
 for item in data['nodes']:
     for dependency in item.get('dependency', []):
+        # check if dependency node exist
+        if not any(dependency == node['id'] for node in data['nodes']):
+            continue
         net.add_edge(dependency, item['id'])
 
 net.show_buttons(filter_=['physics'])
